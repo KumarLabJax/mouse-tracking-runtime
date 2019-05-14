05c369bca00f70d45d4f41e5d6e4cc70ab2d8420
Keith Sheppard <keithshep@gmail.com>
2019-05-14 10:53:07 -0400

    remove mpii and coco import from mouse-of branch

--- lib/dataset/__init__.py
+++ lib/dataset/__init__.py
@@ -8,6 +8,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from .mpii import MPIIDataset as mpii
-from .coco import COCODataset as coco
+# from .mpii import MPIIDataset as mpii
+# from .coco import COCODataset as coco
 from .hdf5mousepose import HDF5MousePose as hdf5mousepose
