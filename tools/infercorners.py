f26f2c60054ba2af6819c0f30ff7fd2a5b3c8eb0
Keith Sheppard <keith.sheppard@jax.org>
2021-09-23 22:27:33 -0400

    infer corners modified for batch

--- tools/infercorners.py
+++ tools/infercorners.py
@@ -145,122 +145,124 @@ def main():
             video_filenames.append(video_filename)
 
     with torch.no_grad(), open(args.batch_file) as batch_file:
-        for line in batch_file:
-            vid_filename = line.strip()
-            if vid_filename:
-                video_filename = os.path.join(args.root_dir, vid_filename)
-                if os.path.isfile(video_filename):
+        for video_filename in video_filenames:
+            if not os.path.isfile(video_filename):
+                print(f'ERROR: {video_filename} is not a valid file')
+                continue
 
-                    with imageio.get_reader(video_filename) as reader:
+            with imageio.get_reader(video_filename) as reader:
 
-                        all_preds = []
-                        all_maxvals = []
-                        batch = []
+                all_preds = []
+                all_maxvals = []
+                batch = []
 
-                        def perform_inference():
-                            if batch:
-                                batch_tensor = torch.stack([xform(img) for img in batch]).cuda()
-                                batch.clear()
+                def perform_inference():
+                    if batch:
+                        batch_tensor = torch.stack([xform(img) for img in batch]).cuda()
+                        batch.clear()
 
-                                x = model(batch_tensor)
+                        x = model(batch_tensor)
 
-                                x.squeeze_(-3)
+                        x.squeeze_(-3)
 
-                                img_h = batch_tensor.size(-2)
-                                img_w = batch_tensor.size(-1)
+                        img_h = batch_tensor.size(-2)
+                        img_w = batch_tensor.size(-1)
 
-                                x_ul = x[:, :(img_h // 2), :(img_w // 2)]
-                                x_ll = x[:, (img_h // 2):, :(img_w // 2)]
-                                x_ur = x[:, :(img_h // 2), (img_w // 2):]
-                                x_lr = x[:, (img_h // 2):, (img_w // 2):]
+                        x_ul = x[:, :(img_h // 2), :(img_w // 2)]
+                        x_ll = x[:, (img_h // 2):, :(img_w // 2)]
+                        x_ur = x[:, :(img_h // 2), (img_w // 2):]
+                        x_lr = x[:, (img_h // 2):, (img_w // 2):]
 
-                                maxvals_ul, preds_ul = argmax_2d(x_ul)
-                                maxvals_ll, preds_ll = argmax_2d(x_ll)
-                                maxvals_ur, preds_ur = argmax_2d(x_ur)
-                                maxvals_lr, preds_lr = argmax_2d(x_lr)
+                        maxvals_ul, preds_ul = argmax_2d(x_ul)
+                        maxvals_ll, preds_ll = argmax_2d(x_ll)
+                        maxvals_ur, preds_ur = argmax_2d(x_ur)
+                        maxvals_lr, preds_lr = argmax_2d(x_lr)
 
-                                preds_ul = preds_ul.cpu().numpy().astype(np.uint16)
-                                preds_ll = preds_ll.cpu().numpy().astype(np.uint16)
-                                preds_ur = preds_ur.cpu().numpy().astype(np.uint16)
-                                preds_lr = preds_lr.cpu().numpy().astype(np.uint16)
+                        preds_ul = preds_ul.cpu().numpy().astype(np.uint16)
+                        preds_ll = preds_ll.cpu().numpy().astype(np.uint16)
+                        preds_ur = preds_ur.cpu().numpy().astype(np.uint16)
+                        preds_lr = preds_lr.cpu().numpy().astype(np.uint16)
 
-                                preds_ll[..., 0] += img_h // 2
-                                preds_ur[..., 1] += img_w // 2
-                                preds_lr[..., 0] += img_h // 2
-                                preds_lr[..., 1] += img_w // 2
+                        preds_ll[..., 0] += img_h // 2
+                        preds_ur[..., 1] += img_w // 2
+                        preds_lr[..., 0] += img_h // 2
+                        preds_lr[..., 1] += img_w // 2
 
-                                pred_stack = np.stack([preds_ul, preds_ll, preds_ur, preds_lr], axis=-2)
+                        pred_stack = np.stack(
+                            [preds_ul, preds_ll, preds_ur, preds_lr],
+                            axis=-2,
+                        )
 
-                                all_preds.append(pred_stack)
+                        all_preds.append(pred_stack)
 
-                        last_frame_index = 600
-                        frame_step_size = 100
-                        for frame_index, image in enumerate(reader):
+                last_frame_index = 600
+                frame_step_size = 100
+                for frame_index, image in enumerate(reader):
 
-                            if frame_index == 0:
-                                mockup = image
+                    if frame_index == 0:
+                        mockup = image
 
-                            if frame_index % frame_step_size == 0:
+                    if frame_index % frame_step_size == 0:
 
-                                batch.append(image)
-                                perform_inference()
+                        batch.append(image)
+                        perform_inference()
 
-                            if frame_index == last_frame_index:
-                                break
+                    if frame_index == last_frame_index:
+                        break
 
-                        all_preds = np.concatenate(all_preds)
+                all_preds = np.concatenate(all_preds)
 
-                        xmed_ul = []
-                        xmed_ll = []
-                        xmed_ur = []
-                        xmed_lr = []
+                xmed_ul = []
+                xmed_ll = []
+                xmed_ur = []
+                xmed_lr = []
 
-                        ymed_ul = []
-                        ymed_ll = []
-                        ymed_ur = []
-                        ymed_lr = []
+                ymed_ul = []
+                ymed_ll = []
+                ymed_ur = []
+                ymed_lr = []
 
-                        for i in range(len(all_preds[0])):
-                            xmed_ul.append(all_preds[i, 0, 1])
-                            xmed_ll.append(all_preds[i, 1, 1])
-                            xmed_ur.append(all_preds[i, 2, 1])
-                            xmed_lr.append(all_preds[i, 3, 1])
+                for i in range(len(all_preds[0])):
+                    xmed_ul.append(all_preds[i, 0, 1])
+                    xmed_ll.append(all_preds[i, 1, 1])
+                    xmed_ur.append(all_preds[i, 2, 1])
+                    xmed_lr.append(all_preds[i, 3, 1])
 
-                            ymed_ul.append(all_preds[i, 0, 0])
-                            ymed_ll.append(all_preds[i, 1, 0])
-                            ymed_ur.append(all_preds[i, 2, 0])
-                            ymed_lr.append(all_preds[i, 3, 0])
+                    ymed_ul.append(all_preds[i, 0, 0])
+                    ymed_ll.append(all_preds[i, 1, 0])
+                    ymed_ur.append(all_preds[i, 2, 0])
+                    ymed_lr.append(all_preds[i, 3, 0])
 
-                        xs = [
-                            int(np.median(xmed_ul)),
-                            int(np.median(xmed_ll)),
-                            int(np.median(xmed_ur)),
-                            int(np.median(xmed_lr)),
-                        ]
-                        ys = [
-                            int(np.median(ymed_ul)),
-                            int(np.median(ymed_ll)),
-                            int(np.median(ymed_ur)),
-                            int(np.median(ymed_lr)),
-                        ]
-                        out_doc = {
-                            'corner_coords': {
-                                'xs': xs,
-                                'ys': ys,
-                            }
-                        }
+                xs = [
+                    int(np.median(xmed_ul)),
+                    int(np.median(xmed_ll)),
+                    int(np.median(xmed_ur)),
+                    int(np.median(xmed_lr)),
+                ]
+                ys = [
+                    int(np.median(ymed_ul)),
+                    int(np.median(ymed_ll)),
+                    int(np.median(ymed_ur)),
+                    int(np.median(ymed_lr)),
+                ]
+                out_doc = {
+                    'corner_coords': {
+                        'xs': xs,
+                        'ys': ys,
+                    }
+                }
 
-                        video_filename_root, _ = os.path.splitext(video_filename)
-                        video_yaml_out_filename = video_filename_root + '_corners_v2.yaml'
-                        print('Writing to:', video_yaml_out_filename)
-                        with open(video_yaml_out_filename, 'w') as video_yaml_out_file:
-                            yaml.safe_dump(out_doc, video_yaml_out_file)
+                video_filename_root, _ = os.path.splitext(video_filename)
+                video_yaml_out_filename = video_filename_root + '_corners_v2.yaml'
+                print('Writing to:', video_yaml_out_filename)
+                with open(video_yaml_out_filename, 'w') as video_yaml_out_file:
+                    yaml.safe_dump(out_doc, video_yaml_out_file)
 
-                        video_png_out_filename = video_filename_root + '_corners_v2.png'
-                        for i in range(4):
-                            rr, cc = skimage.draw.circle(ys[i], xs[i], 5, mockup.shape)
-                            skimage.draw.set_color(mockup, (rr, cc), [255, 0, 0])
-                        skimage.io.imsave(video_png_out_filename, mockup)
+                video_png_out_filename = video_filename_root + '_corners_v2.png'
+                for i in range(4):
+                    rr, cc = skimage.draw.circle(ys[i], xs[i], 5, mockup.shape)
+                    skimage.draw.set_color(mockup, (rr, cc), [255, 0, 0])
+                skimage.io.imsave(video_png_out_filename, mockup)
 
 
 if __name__ == "__main__":
