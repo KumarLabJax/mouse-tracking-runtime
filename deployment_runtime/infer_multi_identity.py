5126dbd2e79c5aa26ecde34515abe997deed74b1
Brian Geuther <Brian.Geuther@jax.org>
2024-05-08 10:34:46 -0400

    Adding in the 2023 model as an option

--- deployment_runtime/infer_multi_identity.py
+++ deployment_runtime/infer_multi_identity.py
@@ -9,7 +9,7 @@ from tfs_inference import infer_multi_identity_tfs
 def main(argv):
 	"""Parse command line arguments."""
 	parser = argparse.ArgumentParser(description='Script that infers a mouse identity model.')
-	parser.add_argument('--model', help='Trained model to infer.', default='social-paper', choices=['social-paper'])
+	parser.add_argument('--model', help='Trained model to infer.', default='social-paper', choices=['social-paper', '2023'])
 	parser.add_argument('--runtime', help='Runtime to execute the model.', default='tfs', choices=['tfs'])
 	vid_or_img = parser.add_mutually_exclusive_group(required=True)
 	vid_or_img.add_argument('--video', help='Video file for processing')
