fa7d8ada57bad1d23c358b7a5a8ede1e304fd18a
Brian Geuther <Brian.Geuther@jax.org>
2024-12-26 16:43:09 -0500

    Major potential rework of single mouse pipeline to allow better handling of flags. Adds single files (with -f) and multiple batch files (-b can be repeated) Adjusts flags to be 0 and 1 (instead of true/false)

--- infer-single-pose-pipeline-v6.sh
+++ infer-single-pose-pipeline-v6.sh
@@ -1,127 +1,62 @@
 #!/bin/bash
-#
-#SBATCH --job-name=infer-singlemouse-pipeline
-#
-#SBATCH --time=6:00:00
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --gres=gpu:1
-#SBATCH --qos=gpu_inference
-#SBATCH --partition=gpu_a100_mig
-#SBATCH --mem=16G
-#SBATCH --output=/projects/kumar-lab/multimouse-pipeline/logs/slurm-%x-%A_%a.out
+shopt -s extglob
 
-# Permanent locations of the singularity images
-SINGULARITY_RUNTIME=/projects/kumar-lab/multimouse-pipeline/deployment-runtime-RHEL9-current.sif
+SINGLE_MOUSE_POSE_SCRIPT="run-single-mouse.sh"
 
-ERROR_STR="ERROR: you need to provide a video file to process. Eg: ./infer-single-pose-pipeline-v6.sh /full/path/movie_list.txt [--include-v2]"
+declare -A flags=()
+files=() batches=()
 
-# Basic function that retries a command up to 5 times
-function retry {
-	local n=1
-	local max=5
-	while true; do
-		"$@" && break || {
-			if [[ $n -lt $max ]]; then
-				((n++))
-				echo "Command failed. Attempt $n/$max:"
-			else
-				echo "The command has failed after $n attempts." >&2
-				return 1
-			fi
-		}
-	done
-}
+while (( $# > 0 )) ; do
+  case $1 in
+	# Handle short options with bundled arguments
+    -[ai][!-]*) set -- "${1:0:2}" "-${1:2}" "${@:2}" ; continue ;;
+	# Split short options with arguments
+    -[bf]?*) set -- "${1:0:2}" "${1:2}" "${@:2}" ; continue ;;
+	# Handle long options with embedded =
+    --@(batch|file)=*) set -- "${1%%=*}" "${1#*=}" "${@:2}" ; continue ;;
 
-# Script is being run by a job in slurm and has been assigned a job ID
-if [[ -n "${SLURM_JOB_ID}" ]]; then
-	#echo "DUMP OF CURRENT ENVIRONMENT:"
-	#env
-	FULL_VIDEO_FILE=`head -n $SLURM_ARRAY_TASK_ID $FULL_VIDEO_FILE_LIST | tail -n 1`
-	echo "Running on node: ${SLURM_JOB_NODELIST}"
-	echo "Assigned GPU: ${CUDA_VISIBLE_DEVICES}"
-	echo "Reading from batch: ${FULL_VIDEO_FILE_LIST}"
-	echo "Running inference on: ${FULL_VIDEO_FILE}"
-	echo "Using the following images:"
-	ls -l ${SINGULARITY_RUNTIME}
-	echo "Slurm job info: "
-	scontrol show job -d ${SLURM_JOB_ID}_${SLURM_ARRAY_TASK_ID}
-	# Force group permissions if default log file used
-	LOG_FILE=/projects/kumar-lab/multimouse-pipeline/logs/slurm-${SLURM_JOB_NAME}-${SLURM_JOB_ID}_${SLURM_ARRAY_TASK_ID}.out
-	if [[ -f "${LOG_FILE}" ]]; then
-		chmod g+wr ${LOG_FILE}
-	fi
-	# Actually get to processing
-	# Only continue if video file is present
-	if [[ -f "${FULL_VIDEO_FILE}" ]]; then
-		# Load up required modules
-		module load apptainer
+	#Argument parsing
+    -a|--auto-clip) (( flags[a]++ )) ;;
+    -i|--include-v2) (( flags[i]++ )) ;;
+    -b|--batch) batches+=( "${2?Missing argument for -b|--batch}" ) ; shift ;;
+    -f|--file) files+=( "${2?Missing argument for -f|--file}" ) ; shift ;;
+    --) shift; break ;;
+    -*) printf >&2 'Unknown option %s\n' "$1" ; exit 1 ;;
+    *) break ;;
+  esac
+  shift
+done
 
-		# Setup some useful variables
-		H5_V2_OUT_FILE="${FULL_VIDEO_FILE%.*}_pose_est_v2.h5"
-		H5_V6_OUT_FILE="${FULL_VIDEO_FILE%.*}_pose_est_v6.h5"
-		FAIL_STATE=false
+# Ignore any remaining arguments silently
+# printf 'Unparsed args: %s\n' "${*@Q}"
 
-		# Pose V2 Inference step
-		echo "Running single mouse pose step:"
-		retry singularity exec --nv "${SINGULARITY_RUNTIME}" python3 /kumar_lab_models/mouse-tracking-runtime/infer_single_pose.py --video "${FULL_VIDEO_FILE}" --out-file "${H5_V6_OUT_FILE}" --batch-size 10
-		FAIL_STATE=$?
+ERROR_STR="""
+ERROR: you need to provide at least one video file to process.
+Usage: ./infer-single-pose-pipeline-v6.sh [-b|--batch movie_list.txt] [-f|--file movie.avi] [-i|--include-v2] [-a|--auto-clip]
+"""
 
-		if [[ ! -z "${INCLUDE_V2}" && "${INCLUDE_V2}" == "true" ]]; then
-			cp "${H5_V6_OUT_FILE}" "${H5_V2_OUT_FILE}"
-		fi
-
-		# Corner Inference step
-		if [[ $FAIL_STATE == 0 ]]; then
-			echo "Running arena corner step:"
-			retry singularity exec --nv "${SINGULARITY_RUNTIME}" python3 /kumar_lab_models/mouse-tracking-runtime/infer_arena_corner.py --video "${FULL_VIDEO_FILE}" --out-file "${H5_V6_OUT_FILE}"
-			FAIL_STATE=$?
-		fi
-
-		# Segmentation Inference step
-		if [[ $FAIL_STATE == 0 ]]; then
-			echo "Running segmentation step:"
-			retry singularity exec --nv "${SINGULARITY_RUNTIME}" python3 /kumar_lab_models/mouse-tracking-runtime/infer_single_segmentation.py --video "${FULL_VIDEO_FILE}" --out-file "${H5_V6_OUT_FILE}"
-			FAIL_STATE=$?
-		fi
-
-		# Fecal Boli Inference step
-		if [[ $FAIL_STATE == 0 ]]; then
-			echo "Running fecal boli inference step:"
-			retry singularity exec --nv "${SINGULARITY_RUNTIME}" python3 /kumar_lab_models/mouse-tracking-runtime/infer_fecal_boli.py --video "${FULL_VIDEO_FILE}" --out-file "${H5_V6_OUT_FILE}"
-			FAIL_STATE=$?
-		fi
-
-		# Cleanup if successful
-		if [[ $FAIL_STATE == 0 ]]; then
-			# rm ${FULL_VIDEO_FILE}
-			echo "Finished video file: ${FULL_VIDEO_FILE}"
-		else
-			rm ${H5_V6_OUT_FILE}
-			echo "Pipeline failed for Video ${FULL_VIDEO_FILE}, Please Rerun."
-		fi
-	else
-		echo "ERROR: could not find video file: ${FULL_VIDEO_FILE}" >&2
-	fi
-else
-	# the script is being run from command line. We should do a self-submit as an array job
-	if [[ -f "${1}" ]]; then
-			# echo "${1} is set and not empty"
-			NUM_VIDEOS=`wc -l < ${1}`
-			# should we also output v2?
-			if [[ -z "${2}" ]]; then
-				INCLUDE_V2="false"
-			elif [[ "${2}" == "--include-v2" ]]; then
-				INCLUDE_V2="true"
-			else
-				echo "${ERROR_STR}" >&2
-				exit 1
-			fi
-			# Here we perform a self-submit
-			echo "Submitting ${NUM_VIDEOS} videos for single mouse pose in: ${1}"
-			sbatch --export=FULL_VIDEO_FILE_LIST="${1}",INCLUDE_V2="${INCLUDE_V2}" --array=1-"$NUM_VIDEOS"%56 "${0}"
-	else
-			echo "${ERROR_STR}" >&2
-			exit 1
-	fi
+if [[ ${#files[@]} -eq 0 && ${#batches[@]} -eq 0 ]]; then
+  echo $ERROR_STR
+  exit 1
 fi
+
+# Submit array jobs for batch files
+for batch in "${batches[@]}"; do
+  if [[ ! -f $batch ]]; then
+	echo "ERROR: Batch file $batch does not exist."
+	continue
+  fi
+  NUM_VIDEOS=$(wc -l < $batch)
+  echo "Submitting ${NUM_VIDEOS} videos for single mouse pose in: ${batch}"
+  sbatch --export=BATCH_FILE="${batch}",INCLUDE_V2="${flags[i]:=0}",AUTO_CLIP="${flags[a]:=0}" --array=1-"$NUM_VIDEOS"%56 "${SINGLE_MOUSE_POSE_SCRIPT}"
+done
+
+# Submit jobs for individual files
+for file in "${files[@]}"; do
+  if [[ ! -f $file ]]; then
+	echo "ERROR: File $file does not exist, skipping processing."
+	continue
+  fi
+  echo "Submitting single mouse pose for: ${file}"
+  sbatch --export=VIDEO_FILE="${file}",INCLUDE_V2="${flags[i]:=0}",AUTO_CLIP="${flags[a]:=0}" "${SINGLE_MOUSE_POSE_SCRIPT}"
+done
---
+++ run-single-mouse.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+#
+#SBATCH --job-name=infer-singlemouse-pipeline
+#
+#SBATCH --time=6:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --gres=gpu:1
+#SBATCH --qos=gpu_inference
+#SBATCH --partition=gpu_a100_mig
+#SBATCH --mem=16G
+#SBATCH --output=/projects/kumar-lab/multimouse-pipeline/logs/slurm-%x-%A_%a.out
+
+# Permanent locations of the singularity images
+SINGULARITY_RUNTIME=/projects/kumar-lab/multimouse-pipeline/deployment-runtime-RHEL9-current.sif
+
+# Basic function that retries a command up to 5 times
+function retry {
+	local n=1
+	local max=5
+	while true; do
+		"$@" && break || {
+			if [[ $n -lt $max ]]; then
+				((n++))
+				echo "Command failed. Attempt $n/$max:"
+			else
+				echo "The command has failed after $n attempts." >&2
+				return 1
+			fi
+		}
+	done
+}
+
+echo "Running on node: ${SLURM_JOB_NODELIST}"
+echo "Assigned GPU: ${CUDA_VISIBLE_DEVICES}"
+echo "Using the following images:"
+ls -l ${SINGULARITY_RUNTIME}
+echo "Slurm job info: "
+if [[ -z "${SLURM_ARRAY_TASK_ID}" ]]; then
+	JOB_STRING="${SLURM_JOB_ID}"
+else
+	JOB_STRING="${SLURM_JOB_ID}_${SLURM_ARRAY_TASK_ID}"
+fi
+
+#scontrol show job -d ${JOB_STRING}
+# Force group permissions if default log file used
+LOG_FILE=/projects/kumar-lab/multimouse-pipeline/logs/slurm-${SLURM_JOB_NAME}-${JOB_STRING}.out
+if [[ -f "${LOG_FILE}" ]]; then
+	chmod g+wr ${LOG_FILE}
+fi
+
+# Check if we are running a batch job or a single job and assign the video file
+if [[ -z "${VIDEO_FILE}" ]]; then
+	# VIDEO_FILE is not set, so we are running an array job
+	if [[ -z "${BATCH_FILE}" || -z "${SLURM_ARRAY_TASK_ID}" ]]; then
+		echo "Video batch file or slurm array task ID not found, exiting."
+		exit 1
+	fi
+	echo "Reading from batch: ${BATCH_FILE}"
+	VIDEO_FILE=`head -n $SLURM_ARRAY_TASK_ID $BATCH_FILE | tail -n 1`
+fi
+
+echo "Processing video file: ${VIDEO_FILE}"
+# Only continue if video file is present
+if [[ -z "${VIDEO_FILE}" ]]; then
+	echo "No video file found for this task, exiting."
+	exit 1
+fi
+
+# Load up required modules
+module load apptainer
+
+# Setup some useful variables
+H5_V2_OUT_FILE="${VIDEO_FILE%.*}_pose_est_v2.h5"
+H5_V6_OUT_FILE="${VIDEO_FILE%.*}_pose_est_v6.h5"
+
+function fail_cleanup() {
+	rm ${H5_V6_OUT_FILE}
+	echo "Error on line $1"
+    echo $( head -n $1 ${0} | tail -n 1 )
+	echo "Pipeline failed for Video ${VIDEO_FILE}, Please Rerun."
+    exit 1
+}
+
+trap 'fail_cleanup $LINENO' ERR
+
+#----------------------------------------------
+# Run the pipeline
+#----------------------------------------------
+
+# Pose V2 Inference step
+echo "Running single mouse pose step:"
+retry singularity exec --nv "${SINGULARITY_RUNTIME}" python3 /kumar_lab_models/mouse-tracking-runtime/infer_single_pose.py --video "${FULL_VIDEO_FILE}" --out-file "${H5_V6_OUT_FILE}" --batch-size 10
+
+# Clip the video file if requested
+singularity exec "${SINGULARITY_RUNTIME}" python3 /kumar_lab_models/mouse-tracking-runtime/clip_video_to_start.py --in-video "${FULL_VIDEO_FILE}" --in-pose "${H5_V6_OUT_FILE}" --out-video "${VIDEO_FILE%.*}_trimmed.${VIDEO_FILE##*.}" --out-pose "${H5_V6_OUT_FILE%.*}_trimmed.h5" auto
+
+# Save copy of V2 output if requested
+if [[ ! -z "${INCLUDE_V2}" && "${INCLUDE_V2}" -eq 1 ]]; then
+	cp "${H5_V6_OUT_FILE}" "${H5_V2_OUT_FILE}"
+fi
+
+# Corner Inference step
+echo "Running arena corner step:"
+retry singularity exec --nv "${SINGULARITY_RUNTIME}" python3 /kumar_lab_models/mouse-tracking-runtime/infer_arena_corner.py --video "${FULL_VIDEO_FILE}" --out-file "${H5_V6_OUT_FILE}"
+
+# Segmentation Inference step
+echo "Running segmentation step:"
+retry singularity exec --nv "${SINGULARITY_RUNTIME}" python3 /kumar_lab_models/mouse-tracking-runtime/infer_single_segmentation.py --video "${FULL_VIDEO_FILE}" --out-file "${H5_V6_OUT_FILE}"
+
+# Fecal Boli Inference step
+echo "Running fecal boli inference step:"
+retry singularity exec --nv "${SINGULARITY_RUNTIME}" python3 /kumar_lab_models/mouse-tracking-runtime/infer_fecal_boli.py --video "${FULL_VIDEO_FILE}" --out-file "${H5_V6_OUT_FILE}"
+
+# Cleanup if successful
+# rm ${VIDEO_FILE}
+echo "Finished video file: ${VIDEO_FILE}"
