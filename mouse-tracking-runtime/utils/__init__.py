f2d713a7ec645621a9d459152d9d1bc332733a1c
Brian <Brian.Geuther@jax.org>
2025-01-08 10:51:53 -0500

    Single mouse clipping (#16)
    * Linting of the clipping script
    
    * Adding inspection functions for pose quality
    
    * Adding manual clipping of videos
    
    * Major potential rework of single mouse pipeline to allow better handling of flags.
    Adds single files (with -f) and multiple batch files (-b can be repeated)
    Adjusts flags to be 0 and 1 (instead of true/false)
    
    * Tweaking of the auto clipping
    
    * Adjusting quotes and feedback
    
    * Adding qa to the pipeline
    
    * Consolidating location of logs
    
    * Adding manual clipping args
    
    * Cleaning up formatting and adding comments
    
    * Syntax fixes
    
    * Bugfix in confidence shapes and adding corner logging to inspection
    
    * Allowing numpy integers
    
    * Fixing syntax errors
    
    * Minor fixes for qa params
    
    * More minor fixes
    
    * Clipping start frame to min of 0
    
    * Sorting some items to the top for easier future editing
    
    * Getting the start frame correct
    
    * Adding safety on finding first frames to report -1 if it never occurs
    
    * Adding filename reporting to the qa
    Fixing jabs confidence reporting
    
    * Changing to full path

--- mouse-tracking-runtime/utils/__init__.py
+++ mouse-tracking-runtime/utils/__init__.py
@@ -1,6 +1,38 @@
-from .pose import argmax_2d, localmax_2d, render_pose_overlay, convert_v2_to_v3, convert_multi_to_v2, find_first_pose, find_first_pose_file
+from .pose import (
+    argmax_2d,
+    convert_multi_to_v2,
+    convert_v2_to_v3,
+    find_first_pose,
+    find_first_pose_file,
+    inspect_pose_v2,
+    inspect_pose_v6,
+    localmax_2d,
+    render_pose_overlay,
+)
 from .prediction_saver import prediction_saver
-from .segmentation import get_contours, pad_contours, get_trimmed_contour, get_contour_stack, get_frame_masks, render_blob, get_frame_outlines, render_outline, render_segmentation_overlay
-from .static_objects import plot_keypoints, measure_pair_dists, filter_square_keypoints
+from .segmentation import (
+    get_contour_stack,
+    get_contours,
+    get_frame_masks,
+    get_frame_outlines,
+    get_trimmed_contour,
+    pad_contours,
+    render_blob,
+    render_outline,
+    render_segmentation_overlay,
+)
+from .static_objects import filter_square_keypoints, measure_pair_dists, plot_keypoints
 from .timers import time_accumulator
-from .writers import InvalidPoseFileException, adjust_pose_version, write_pose_v2_data, write_pose_v3_data, write_pose_v4_data, write_identity_data, write_seg_data, write_static_object_data, write_pixel_per_cm_attr, write_fecal_boli_data, write_pose_clip
+from .writers import (
+    InvalidPoseFileException,
+    adjust_pose_version,
+    write_fecal_boli_data,
+    write_identity_data,
+    write_pixel_per_cm_attr,
+    write_pose_clip,
+    write_pose_v2_data,
+    write_pose_v3_data,
+    write_pose_v4_data,
+    write_seg_data,
+    write_static_object_data,
+)
